e46f77aa33a799cf47c32dc9a9d8c48f03c270cf
shubanms <msshuban4@gmail.com>
2024-08-01 10:21:24 +0530

    Fixed deployment startup

--- main.py
+++ main.py
@@ -16,8 +16,3 @@ def hello_world(input: HelloWorldInput):
     logger.info("Called end-point ")
     response = HelloWorldOutput(text=input.text)
     return response
-
-
-if __name__ == "__main__":
-    subprocess.run("pytest")
-    uvicorn.run("main:app", reload=True)
